aa19879e9e717036f552823775ab3ac97b698c5d
tei-ship-it <tei@wiseai.co.kr>
2026-06-02 09:34:33 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -19,7 +18,6 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify(req.body),
     });
-
     const data = await response.json();
     if (!response.ok) return res.status(response.status).json(data);
     return res.status(200).json(data);
